83758e46bb3b8aa59bb98fadaa4d9de44cdf343f
aaliaksandrau <a.aliaksandrau@elinext.com>
2023-02-18 19:49:56 +0400

    refactor: album service remove unused code

--- src/album/album.service.ts
+++ src/album/album.service.ts
@@ -8,7 +8,6 @@ import { CreateAlbumDto } from './dto/create-album.dto';
 import { UpdateAlbumDto } from './dto/update-album.dto';
 import { v4 as uuidv4, validate } from 'uuid';
 import { Album } from './entities/album.entity';
-import { DataObj } from 'src/data';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm/repository/Repository';
 
@@ -27,15 +26,13 @@ export class AlbumService {
     }
 
     const id = uuidv4();
-    const createdAlbum = await this.albumRepository.create({
+    const createdAlbum = this.albumRepository.create({
       id,
       name,
       artistId,
       year,
     });
 
-    // const album = new Album(id, name, year, artistId);
-    // DataObj.albumsData.push(album);
     return await this.albumRepository.save(createdAlbum);
   }
 
@@ -43,11 +40,14 @@ export class AlbumService {
     return await this.albumRepository.find();
   }
 
-  async getById(albumId: string) {
-    if (!validate(albumId)) {
+  private validateId(id: string) {
+    if (!validate(id)) {
       throw new HttpException('Not valid album id', HttpStatus.BAD_REQUEST);
     }
-    // const findAlbum = DataObj.albumsData.find((user) => user.id === albumId);
+  }
+
+  async getById(albumId: string) {
+    this.validateId(albumId);
 
     const findAlbum = await this.albumRepository.findOne({
       where: { id: albumId },
@@ -60,18 +60,8 @@ export class AlbumService {
     return findAlbum;
   }
 
-  async update(albumUniqueId: string, dto: UpdateAlbumDto) {
-    if (!validate(albumUniqueId)) {
-      throw new HttpException('Not valid album id', HttpStatus.BAD_REQUEST);
-    }
-
-    // const index = DataObj.albumsData.findIndex(
-    //   (album) => album.id == albumUniqueId,
-    // );
-
-    // if (index === -1) {
-    //   throw new NotFoundException('album not found.');
-    // }
+  async update(albumId: string, dto: UpdateAlbumDto) {
+    this.validateId(albumId);
 
     if (!dto.name || typeof dto.year !== 'number') {
       throw new HttpException(
@@ -83,46 +73,20 @@ export class AlbumService {
       );
     }
 
-    const updatedAlbum = await this.albumRepository.findOne({
-      where: { id: albumUniqueId },
-    });
-
-    if (!updatedAlbum) {
-      throw new NotFoundException('album not found.');
-    }
+    const updatedAlbum = await this.getById(albumId);
 
     Object.assign(updatedAlbum, dto);
 
     return await this.albumRepository.save(updatedAlbum);
-    // const { id, name, artistId, year } = DataObj.albumsData[index];
-
-    // DataObj.albumsData[index] = new Album(
-    //   id,
-    //   dto.name || name,
-    //   dto.year || year,
-    //   dto.artistId || artistId || null,
-    // );
-    // return DataObj.albumsData[index];
   }
 
   async delete(albumId: string) {
-    if (!validate(albumId)) {
-      throw new HttpException('Not valid album id', HttpStatus.BAD_REQUEST);
-    }
-    // const filteredAlbums = DataObj.albumsData.filter(
-    //   (album) => album.id != albumId,
-    // );
-
-    // if (DataObj.albumsData.length !== filteredAlbums.length) {
-    //   DataObj.albumsData = filteredAlbums;
-    // } else {
-    //   throw new NotFoundException('Album not found.');
-    // }
+    this.validateId(albumId);
 
     const result = await this.albumRepository.delete({ id: albumId });
 
     if (result.affected === 0) {
-      throw new NotFoundException('User not found.');
+      throw new NotFoundException('Album not found.');
     }
   }
 }
