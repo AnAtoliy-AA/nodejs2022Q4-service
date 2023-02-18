2a3acc5e3116818fd90f651e98250ac01125d428
aaliaksandrau <a.aliaksandrau@elinext.com>
2023-02-18 19:37:54 +0400

    refactor: artist service remove unused code

--- src/artist/artist.service.ts
+++ src/artist/artist.service.ts
@@ -15,7 +15,7 @@ import { Repository } from 'typeorm';
 export class ArtistService {
   constructor(
     @InjectRepository(Artist)
-    private artistRepository: Repository<Artist>
+    private artistRepository: Repository<Artist>,
   ) {}
 
   async create(dto: CreateArtistDto) {
@@ -26,23 +26,17 @@ export class ArtistService {
     }
 
     const id = uuidv4();
-    // const artist = new Artist(id, name, grammy);
-    // DataObj.artistsData.push(artist);
 
-    // return artist;
     const createdArtist = this.artistRepository.create({
       id,
       name,
       grammy,
     });
 
-    // const album = new Album(id, name, year, artistId);
-    // DataObj.albumsData.push(album);
     return await this.artistRepository.save(createdArtist);
   }
 
   async findAll() {
-    // return DataObj.artistsData;
     return await this.artistRepository.find();
   }
 
@@ -54,13 +48,7 @@ export class ArtistService {
 
   async getById(artistId: string) {
     this.validateId(artistId);
-    // const artist = DataObj.artistsData.find((artist) => artist.id == artistId);
 
-    // if (!artist) {
-    //   throw new NotFoundException('Artist not found.');
-    // }
-
-    // return artist;
     const artist = await this.artistRepository.findOne({
       where: { id: artistId },
     });
@@ -92,50 +80,10 @@ export class ArtistService {
     Object.assign(updatedArtist, dto);
 
     return await this.artistRepository.save(updatedArtist);
-
-    // const index = DataObj.artistsData.findIndex(
-    //   (artist) => artist.id == artistUniqueId,
-    // );
-
-    // if (index === -1) {
-    //   throw new NotFoundException('artist not found.');
-    // }
-
-    // const { id, name, grammy } = DataObj.artistsData[index];
-
-    // const updatedName =
-    //   dto.hasOwnProperty('name') && dto?.name !== undefined ? dto.name : name;
-    // const updatedGrammy =
-    //   dto.hasOwnProperty('grammy') && dto?.grammy !== undefined
-    //     ? dto.grammy
-    //     : grammy;
-
-    // DataObj.artistsData[index] = new Artist(id, updatedName, updatedGrammy);
-
-    // return DataObj.artistsData[index];
   }
 
   async delete(artistId: string) {
     this.validateId(artistId);
-
-    // const tracks = await this.trackService?.findAll();
-    // if (tracks?.length) {
-    //   tracks.forEach(async (_track) => {
-    //     if (_track?.artistId === artistId) {
-    //       await this.trackService?.resetArtistId(_track?.id, artistId);
-    //     }
-    //   });
-    // }
-
-    // const filteredArtists = DataObj.artistsData.filter(
-    //   (artist) => artist.id != artistId,
-    // );
-
-    // if (DataObj.artistsData.length !== filteredArtists.length) {
-    //   DataObj.artistsData = filteredArtists;
-    // } else {
-    //   throw new NotFoundException('Artist not found.');
-    // }
 
     const result = await this.artistRepository.delete({ id: artistId });
 
